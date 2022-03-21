e9e235952c50c9fd5b6a2eaaeff8802dca6c7b1e
XxmunichxX <69469575+XxmunichxX@users.noreply.github.com>
2022-03-21 22:40:14 +0100

    up

--- SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
+++ SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
@@ -9,27 +9,36 @@ import SwiftUI
 
 struct PortfolioView: View {
     
-    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject private var vm: HomeViewModel
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
+                    SearchBarView(searchText: $vm.searchText)
                     
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        LazyHStack(spacing: 10) {
+                            ForEach(vm.allCoins) { coin in
+                                Text(coin.symbol.uppercased())
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                Button(action: {presentationMode.wrappedValue.dismiss()}) {
-                    Image(systemName: "xmark")
-                }
+                XMarkButton()
             }
         }
     }
 }
 
+
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
     }
 }
